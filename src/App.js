097c0331b91f0fcfe6624704b9fe275c1d51853e
Jennifer Peavler <jenn@crafted.education>
2019-06-16 23:39:46 -0600

    initial cra and react dnd setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { DndProvider, DragDropContext, useDrag, useDrop } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+
+import { DragItemTypes } from './DragItemTypes';
+
+
+import './App.scss';
 
 function App() {
+  const [{isDragging}, drag] = useDrag({
+    item: { type: DragItemTypes.DIV },
+		collect: monitor => ({
+			isDragging: !!monitor.isDragging(),
+		}),
+  });
+
+
+
+  const [, drop] = useDrop({
+    accept: DragItemTypes.DIV,
+    drop: () => console.log('i dropped a div')
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DndProvider backend={HTML5Backend}>
+      <div className='App'>
+        <aside className='App__element-list'>
+          <div ref={drag} style={{opacity: isDragging ? 0.5 : 1, cursor: 'move'}}>div</div>
+        </aside>
+        <main className='App__workpage' ref={drop}>
+        
+        </main>
+      </div>
+    </DndProvider>
   );
 }
 
-export default App;
+export default DragDropContext(HTML5Backend)(App);
